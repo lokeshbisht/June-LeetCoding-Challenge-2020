c4a76c6a011dfe61c1290c743e50f4ea45f5db87
lokeshbisht <31874727+lokeshbisht@users.noreply.github.com>
2020-06-09 16:28:16 +0530

    Day 9

---
+++ Problems/Is Subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        int i = 0;
+        
+        for (char c : t) {
+            if (c == s[i]) {
+                i++;
+            }
+        }
+        
+        return (i == s.length());
+    }
+};

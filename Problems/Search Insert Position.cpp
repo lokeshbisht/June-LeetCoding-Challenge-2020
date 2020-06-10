5f3695e13262b8961bc3263f0582d2b5bad47ba5
lokeshbisht <31874727+lokeshbisht@users.noreply.github.com>
2020-06-10 23:38:49 +0530

    Day 10

---
+++ Problems/Search Insert Position.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int pos;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                pos = i;
+                break;
+            } else if (nums[i] > target) {
+                pos = i;
+                break;
+            }
+        }
+        
+        return pos;
+    }
+};
